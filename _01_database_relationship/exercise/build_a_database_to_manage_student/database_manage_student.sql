7e567a7cdb6a8f63f1fffdddb95f3a1cdbfac1b2
NHTAirXonix <toan02041999@gmail.com>
2022-05-16 08:24:14 +0700

    update

--- _01_database_relationship/exercise/build_a_database_to_manage_student/database_manage_student.sql
+++ _01_database_relationship/exercise/build_a_database_to_manage_student/database_manage_student.sql
@@ -1,24 +1,30 @@
-CREATE DATABASE `student_management`;
-
+CREATE DATABASE IF NOT EXISTS `student_management`;
 USE student_management;
 
--- DROP DATABASE student_management;
-
-CREATE TABLE student (
+CREATE TABLE IF NOT EXISTS student (
     id INT,
-    nameOfStudent CHARACTER(255),
+    name_of_student CHARACTER(255),
     age INT,
     country CHARACTER(255)
 );
 
-CREATE TABLE class (
-    idOfClass INT,
+CREATE TABLE IF NOT EXISTS class (
+    id_of_class INT,
     nameOfClass CHARACTER(255)
 );
 
-CREATE TABLE teacher (
+CREATE TABLE IF NOT EXISTS teacher (
     id INT,
-    nameOfTeacher CHARACTER(255),
+    name_of_teacher CHARACTER(255),
     age INT,
     country CHARACTER(255)
 );
+
+insert into class values (1,"MATH");
+insert into class values (2,"HISTORY");
+
+insert into student values (1,"Hao",18,'viet nam');
+insert into student values (2,"Phong",19,'lao');
+
+insert into teacher values (1,"Tien",24,'viet nam');
+insert into teacher values (2,"Chien",25,'vietnam');
